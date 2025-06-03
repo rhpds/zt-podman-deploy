54419f1dd398d7dcf374b74858d8ffec3f6cc733
Matthew Yee <myee@redhat.com>
2025-06-03 09:15:43 -0700

    got rid of some script cruft

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
-while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
-do
-    echo "Waiting for Instruqt to finish booting the VM"
-    sleep 1
-done
-
-subscription-manager config --rhsm.manage_repos=1
-subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
 
 dnf install -y tree buildah podman
-
-echo "Adding wheel" > /root/post-run.log
-usermod -aG wheel rhel
-
-echo "setting password" >> /root/post-run.log
-echo redhat | passwd --stdin rhel
-
-echo "DONE" >> /root/post-run.log
 
 set -ex
 
@@ -39,24 +23,3 @@ buildah config --port 80 --cmd "/usr/sbin/httpd -DFOREGROUND" ${newcontainer}
 # commit the image
 buildah unmount ${newcontainer}
 buildah commit ${newcontainer} rhel9-httpd
-
-
-#set up tmux so it has to restart itself whenever the system reboots
-
-#step 1: make a script
-tee ~/startup-tmux.sh << EOF
-TMUX='' tmux new-session -d -s 'podman' > /dev/null 2>&1
-TMUX='' tmux new-session -d -s 'host' > /dev/null 2>&1
-tmux set -g pane-border-status top
-tmux setw -g pane-border-format ' #{pane_index} #{pane_current_command}'
-tmux set -g mouse on
-tmux set mouse on
-EOF
-
-#step 2: make it executable
-chmod +x ~/startup-tmux.sh
-#step 3: use cron to execute 
-echo "@reboot ~/startup-tmux.sh" | crontab -
-
-#step 4: start tmux for the lab
-~/startup-tmux.sh
