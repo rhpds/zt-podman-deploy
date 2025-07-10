7e636b7ffd3f0e8f174c03a4960b07c48e9f21c6
Matt Micene (RHT) <mmicene@redhat.com>
2025-07-10 16:15:44 -0400

    RHEL 10 update for image and container

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-dnf install -y tree buildah podman
-
 set -ex
 
 # start new container from scratch
@@ -9,7 +7,10 @@ newcontainer=$(buildah from scratch)
 scratchmnt=$(buildah mount ${newcontainer})
 
 # install the packages
-yum install --installroot ${scratchmnt} httpd --releasever 9 --setopt=module_platform_id="platform:el9" -y
+dnf install -y --releasever=10 --installroot=$scratchmnt redhat-release
+dnf install -y --setopt=reposdir=/etc/yum.repos.d \
+      --installroot=$scratchmnt \
+      --setopt=cachedir=/var/cache/dnf httpd
 
 # Clean up yum cache
 if [ -d "${scratchmnt}" ]; then
@@ -17,9 +18,9 @@ if [ -d "${scratchmnt}" ]; then
 fi
 
 # configure container label and entrypoint
-buildah config --label name=rhel9-httpd ${newcontainer}
+buildah config --label name=rhel10-httpd ${newcontainer}
 buildah config --port 80 --cmd "/usr/sbin/httpd -DFOREGROUND" ${newcontainer}
 
 # commit the image
 buildah unmount ${newcontainer}
-buildah commit ${newcontainer} rhel9-httpd
+buildah commit ${newcontainer} rhel10-httpd
