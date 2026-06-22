40f7840662f7938742982fa76068c29985ba4af1
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 10:23:20 -0400

    Unregister and re-register the system

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Unregister and register the VM
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+
 set -ex
 
 # start new container from scratch
